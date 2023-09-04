5f27a5fb94f35174a5fb539580d5a1e49d16f95a
gabriel <ga.dearaujo4@gmail.com>
2023-09-04 00:16:28 -0300

    Melhorias
    -Adicionado filtro por Status
    
    -Adicionado texto do status
    
    -Adicionado aba de pesquisa(apenas visual)

--- lib/main.dart
+++ lib/main.dart
@@ -35,12 +35,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool? filter = false;
+  bool? checkBoxAll = false;
+  bool? checkBoxToSell = true;
+
   @override
   void initState() {
     super.initState();
   }
 
-  Future<List<BookModel>> getProductList() async {
+  Future<List<BookModel>> getBookList() async {
+    String productURl = '$SERVER_URL/book/active';
+
+    final response = await http.get(Uri.parse(productURl),
+        headers: {"Content-Type": "application/json"});
+    List jsonResponse = json.decode(response.body);
+
+    return jsonResponse.map((job) => BookModel.fromJson(job)).toList();
+  }
+
+  Future<List<BookModel>> getAllBookList() async {
     String productURl = '$SERVER_URL/book';
 
     final response = await http.get(Uri.parse(productURl),
@@ -56,54 +70,276 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
         centerTitle: true,
+        elevation: 0.0,
       ),
       body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                height: MediaQuery.of(context).size.height - 80,
-                child: FutureBuilder<List<BookModel>>(
-                  future: getProductList(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: GridView.builder(
-                              itemCount: snapshot.data!.length,
-                              gridDelegate:
-                                  const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 2,
+        child: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Material(
+                  elevation: 5,
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.indigo,
+                    child: Column(children: [
+                      TextFormField(
+                        style: const TextStyle(color: Colors.white),
+                        cursorColor: Colors.white,
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          hintText: 'Pesquisar mais livros...',
+                          icon: const Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
+                          contentPadding: const EdgeInsets.only(left: 10),
+                          hintStyle: const TextStyle(color: Colors.white),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                const BorderSide(width: 1, color: Colors.white),
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                const BorderSide(width: 1, color: Colors.white),
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          showDialogFilter();
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            filter!
+                                ? Text(
+                                    'Filtros: Todos',
+                                    style: TextStyle(
+                                      color: Colors.white.withOpacity(0.4),
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 13.0,
+                                    ),
+                                  )
+                                : Container(),
+                            const Spacer(),
+                            const Text(
+                              'Filtrar',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.normal,
                               ),
-                              itemBuilder: (BuildContext context, int i) {
-                                return Card(
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        border: Border.all(
-                                            width: 0.5, color: Colors.grey)),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Column(
-                                        children: <Widget>[
-                                          Text(snapshot.data![i].name!),
-                                          Text(
-                                              'R\$ ${snapshot.data![i].price!.toString()}'),
-                                        ],
+                            ),
+                            const Icon(
+                              Icons.filter_alt_outlined,
+                              color: Colors.white,
+                            )
+                          ],
+                        ),
+                      ),
+                    ]),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Divider(
+                  color: Colors.indigo,
+                  indent: 20.0,
+                  endIndent: 20.0,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  child: FutureBuilder<List<BookModel>>(
+                    future: filter! ? getAllBookList() : getBookList(),
+                    builder: (context, snapshot) {
+                      // ignore: unrelated_type_equality_checks
+                      if (snapshot.inState(ConnectionState.waiting) == true) {
+                        return const Center(
+                          child: CircularProgressIndicator(color: Colors.black),
+                        );
+                      } else if (snapshot.hasData) {
+                        return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: GridView.builder(
+                                itemCount: snapshot.data!.length,
+                                padding: const EdgeInsets.only(top: 8.0),
+                                physics: const NeverScrollableScrollPhysics(),
+                                gridDelegate:
+                                    const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                ),
+                                itemBuilder: (BuildContext context, int i) {
+                                  return Card(
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              width: 0.5, color: Colors.grey)),
+                                      child: Padding(
+                                        padding: EdgeInsets.only(
+                                            top: filter! ? 0.0 : 8.0),
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: <Widget>[
+                                            Text(
+                                              snapshot.data![i].name!,
+                                              style: const TextStyle(
+                                                color: Colors.indigo,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              'R\$ ${snapshot.data![i].price!.toString()}',
+                                              style: const TextStyle(
+                                                color: Colors.indigo,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            const Spacer(),
+                                            Image.network(
+                                              'https://img.freepik.com/psd-premium/modelo-de-capa-de-livro_125540-572.jpg?w=2000',
+                                              fit: BoxFit.fill,
+                                            ),
+                                            filter!
+                                                ? Container(
+                                                    alignment: Alignment.center,
+                                                    color: snapshot
+                                                            .data![i].status!
+                                                            .contains('VENDIDO')
+                                                        ? Colors.yellow
+                                                        : snapshot.data![i]
+                                                                .status!
+                                                                .contains(
+                                                                    'CANCELADO')
+                                                            ? Colors.red
+                                                            : Colors.green,
+                                                    height: 20,
+                                                    child: Text(
+                                                      snapshot.data![i].status!,
+                                                      style: TextStyle(
+                                                          color: snapshot
+                                                                  .data![i]
+                                                                  .status!
+                                                                  .contains(
+                                                                      'VENDIDO')
+                                                              ? Colors.black
+                                                              : Colors.white),
+                                                    ),
+                                                  )
+                                                : Container()
+                                          ],
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                );
-                              }));
-                    } else if (snapshot.hasError) {
-                      return Text("${snapshot.error}");
-                    } else {
-                      return Container();
-                    }
-                  },
+                                  );
+                                }));
+                      } else if (snapshot.hasError) {
+                        return const Text("Erro ao conectar no banco.");
+                      } else {
+                        return Container();
+                      }
+                    },
+                  ),
                 ),
-              ),
-            ]),
+              ]),
+        ),
       ),
     );
+  }
+
+  void showDialogFilter() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(builder: (context, setStateDialog) {
+            return AlertDialog(
+              title: const Text(
+                'Filtrar',
+                style: TextStyle(color: Colors.white),
+              ),
+              content: Column(mainAxisSize: MainAxisSize.min, children: [
+                CheckboxListTile(
+                  value: checkBoxToSell,
+                  side: const BorderSide(width: 2.0, color: Colors.white),
+                  checkboxShape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  checkColor: Colors.indigo,
+                  activeColor: Colors.white,
+                  onChanged: (value) {
+                    setStateDialog(() {
+                      checkBoxToSell = value;
+                      checkBoxAll = false;
+                    });
+                  },
+                  title: const Text(
+                    'À venda',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Divider(
+                  endIndent: 10,
+                  indent: 10,
+                  color: Colors.white.withOpacity(0.2),
+                ),
+                CheckboxListTile(
+                  value: checkBoxAll,
+                  side: const BorderSide(width: 2.0, color: Colors.white),
+                  checkboxShape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  checkColor: Colors.indigo,
+                  activeColor: Colors.white,
+                  onChanged: (value) {
+                    setStateDialog(() {
+                      checkBoxAll = value;
+                      checkBoxToSell = false;
+                    });
+                  },
+                  title: const Text(
+                    'Todos',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ]),
+              backgroundColor: Colors.indigo,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              actions: <Widget>[
+                ElevatedButton(
+                  style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(Colors.white)),
+                  child: const Text(
+                    'Filtrar',
+                    style: TextStyle(color: Colors.indigo),
+                  ),
+                  onPressed: () {
+                    setStateDialog(() {
+                      if (checkBoxAll!) {
+                        filter = true;
+                      } else {
+                        filter = false;
+                      }
+                      Navigator.pop(context);
+                      setState(() {});
+                    });
+                  },
+                ),
+                TextButton(
+                  child: const Text(
+                    'Voltar',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            );
+          });
+        });
   }
 }
