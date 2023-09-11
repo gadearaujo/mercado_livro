ca91f56fcb680dc9d2aebbe55a09b1ac87b94bcf
gabriel <ga.dearaujo4@gmail.com>
2023-09-10 21:02:39 -0300

    add campos photo url e password customer model

--- lib/service/model/customer_model.dart
+++ lib/service/model/customer_model.dart
@@ -5,17 +5,24 @@ class CustomerModel {
 
   final String? email;
 
+  final String? password;
+
+  final String? photoUrl;
+
   const CustomerModel({
     this.id,
     this.name,
     this.email,
+    this.password,
+    this.photoUrl,
   });
 
   factory CustomerModel.fromJson(Map<String, dynamic> json) {
     return CustomerModel(
-      id: json['id'],
-      name: json['name'],
-      email: json['email'],
-    );
+        id: json['id'],
+        name: json['name'],
+        email: json['email'],
+        password: json['password'],
+        photoUrl: json['photoUrl']);
   }
 }
